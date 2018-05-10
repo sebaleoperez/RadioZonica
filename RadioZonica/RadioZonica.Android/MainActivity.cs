33627b7beae946c4f611f1633a07d3cde3ac90ff
Ilan <ilanolkies@outlook.com>
2018-05-10 01:20:57 -0300

    Add Firebase Analytics to Android app.

--- RadioZonica/RadioZonica.Android/MainActivity.cs
+++ RadioZonica/RadioZonica.Android/MainActivity.cs
@@ -1,6 +1,7 @@
 ﻿using Android.App;
 using Android.Content.PM;
 using Android.OS;
+using Firebase.Analytics;
 
 namespace RadioZonica.Droid
 {
@@ -13,6 +14,9 @@ namespace RadioZonica.Droid
 
             TabLayoutResource = Resource.Layout.Tabbar;
             ToolbarResource = Resource.Layout.Toolbar;
+
+			var firebase = FirebaseAnalytics.GetInstance(this);
+			firebase.LogEvent(FirebaseAnalytics.Event.AppOpen, null);
 
             base.OnCreate(bundle);
 
